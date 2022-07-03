7d945a5bc84742a26c1c9e56a6f47134e3e4d026
Sal4mander <kamran.gasimov27@gmail.com>
2022-07-04 00:41:06 +0200

    added comment to the init in main to explain why it's needed

--- main.go
+++ main.go
@@ -13,8 +13,11 @@ func main() {
 
 func init() {
 	cmd.TodoList = &task.List{}
+	// Since os.WriteFile truncates / overwrites the file it's writing to, it's important to
+	// load the List with data on init to be able to save multiple todos to the file.
 	err := cmd.TodoList.Load(".todo.json")
 	if err != nil {
+		// If the app is run for the first time, a json file to store the todos is created.
 		if err == os.ErrNotExist {
 			_ = cmd.TodoList.Add("Used to create the json file. Content won't be saved.")
 		} else {
